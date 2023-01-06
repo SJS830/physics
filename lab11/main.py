8bea2adf78d0d480ee5e8f6b774307f14814c2c5
Simon Shkolnik <simonjs830@gmail.com>
2023-01-06 09:04:24 -0500

    Lab 11 changes

--- lab11/main.py
+++ lab11/main.py
@@ -18,20 +18,23 @@ peaks, _ = find_peaks(pos)
 peaks = peaks[np.where(pos[peaks] > .0015)]
 period = np.mean(np.diff(time[peaks]))
 
-def fitting_function(t, alpha):
-    return amplitude * np.exp(-alpha * t)
+def fitting_function(t, alpha, alphaDecay):
+    return amplitude * np.exp(-alpha * np.exp(-alphaDecay * t) * t)
 
 popt, pcov = curve_fit(fitting_function, time[peaks], pos[peaks])
-alpha = popt[0]
+alpha, alphaDecay = popt
 
 print("Amplitude:", amplitude)
 print("Equilibrium:", equilibrium)
 print("Period:", period)
 print("Alpha:", alpha)
+print("Alpha Decay:", alphaDecay)
 
-plt.title(f"Amplitude: {amplitude:.2f}, Equilibrium: {equilibrium:.2f}, Period: {period:.2f}, Alpha: {alpha:.2f}")
+plt.title(f"Amplitude: {amplitude:.4f}, Equilibrium: {equilibrium:.4f}, Period: {period:.4f}, Alpha: {alpha:.4f}, Alpha Decay: {alphaDecay:.4f}")
+plt.xlabel("Time [s]")
+plt.ylabel("Distance from equilibrium [m]")
 plt.plot(time, pos, color="red")
 plt.scatter(time[peaks], pos[peaks], color="orange")
-plt.plot(time, amplitude * np.exp(-alpha * time), color="blue")
+plt.plot(time, fitting_function(time, *popt), color="blue")
 
 plt.show()
